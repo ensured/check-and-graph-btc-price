c781fdd7098ff1dcf0c42c1a1ef321fe647b09aa
SomeOrdinaryUser <74529637+SomeOrdinaryUser@users.noreply.github.com>
2020-12-27 17:54:41 -0800

    awesome

--- btc.py
+++ btc.py
@@ -1,51 +1,40 @@
 import requests
 from time import sleep
 import datetime
+import os
 import getpass
 
-print('''
+user = getpass.getuser()
 
-  _       _                             _                          _                     _                     __  _   _   ____    ____   __  
- | |__   | |_    ___     _ __    _ __  (_)   ___    ___      ___  | |__     ___    ___  | | __   ___   _ __   / / | | | | / ___|  |  _ \  \ \ 
- | '_ \  | __|  / __|   | '_ \  | '__| | |  / __|  / _ \    / __| | '_ \   / _ \  / __| | |/ /  / _ \ | '__| | |  | | | | \___ \  | | | |  | |
- | |_) | | |_  | (__    | |_) | | |    | | | (__  |  __/   | (__  | | | | |  __/ | (__  |   <  |  __/ | |    | |  | |_| |  ___) | | |_| |  | |
- |_.__/   \__|  \___|   | .__/  |_|    |_|  \___|  \___|    \___| |_| |_|  \___|  \___| |_|\_\  \___| |_|    | |   \___/  |____/  |____/   | |
-                        |_|                                                                                   \_\                         /_/ 
+os.makedirs("C:\\Users\\" + user + "\\Desktop\\btc\\all_data", exist_ok=True)
+btc_data_path = "C:\\Users\\" + user + "\\Desktop\\btc\\all_data"
 
-''')
-current_user = getpass.getuser()
-print(f"    Welcome {current_user}!!\n".upper())
-print('''
-    This script I made checks the bitcoin price roughly once every second
-    and conveniently logs it to a .txt file and prints it to the screen.\n
-    Entering 10000 will check the price 1 million times :P
-    Entering .02 will check the price 2 times.\n\n
-''')
 
 def GET_RATES():
-    url = requests.get('http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken')
+    url = requests.get("http://preev.com/pulse/units:btc+usd/sources:bitstamp+kraken")
     data = url.json()
     result = {}
-    for exchange, rates in data['btc']['usd'].items():
-        result[exchange] = float(rates['last'])
+    for rates in data["btc"]["usd"].values():
+        result = float(rates["last"])
     return result
 
 
 def log_and_print_GET_RATES():
-    user_run_amount = float(input("Enter .01 or >: "))
-    user_run_amount = user_run_amount * 100
-    while user_run_amount > 0:
-        chosen_file = "btc_price.txt" # specify where you want to save btc price.
-        my_file = open(chosen_file, "a")
+    v = 10
+    while v > 0:
         now = datetime.datetime.now()
-        now = now.replace(microsecond=0) # datetime.now without microsecond.
-        print(now, GET_RATES(), file=my_file) # Logs time and current btc price to a file.
-        print(now, GET_RATES())
-        sleep(.5)
-        user_run_amount -= 1
+        dt = datetime.datetime.today()
+        date = str(dt.year) + "-" + str(dt.month) + "-" + str(dt.day) + ","
+        date2 = str(dt.year) + "-" + str(dt.month) + "-" + str(dt.day)
+        formatted_date = date + now.strftime("%I:%M:%S%p")
+        print(
+            str(formatted_date) + "," + str(GET_RATES()),
+            file=open(f"{btc_data_path}\\btc_prices_{date2}.csv", "a+"),
+        )  # Logs time and current btc price to a file.
+        print(str(formatted_date) + "," + str(GET_RATES()))
+        sleep(0.5)
     else:
-        print(f'While loop ended and reached "{user_run_amount}"')
-        my_file.close()
-        print("Closed file. ")
+        print("")
+
 
 log_and_print_GET_RATES()
